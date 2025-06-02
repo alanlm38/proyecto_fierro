02a55a41e07396253e9432d3975548b973827993
JJ5A <nunezjulian505@gmail.com>
2025-06-02 00:33:02 -0600

    cambio de nombre de archivo para windows

--- server/dist/config/swagger.d.ts
+++
@@ -1,5 +0,0 @@
-import { SwaggerUiOptions } from 'swagger-ui-express';
-declare const swaggerSpec: object;
-declare const swaggerUiOptions: SwaggerUiOptions;
-export default swaggerSpec;
-export { swaggerUiOptions };
